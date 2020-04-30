bd7be18c98fe30a1c03e1a3963a6e85e228e0e5d
Kunter <kuntaevert@gmail.com>
2020-05-01 01:49:58 +0300

    router config

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ app.use(
 			'http://localhost:3001',
 			'http://localhost:3000',
 			'http://localhost:3002',
-			'https://www.devert.ee',
+			'http://www.devert.ee',
 			'https://www.devert.ee'
 		]
 	})
