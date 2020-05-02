82b6fd642ccdcb215ff017d59c82d3310822c383
kuntervert <kuntaevert@gmail.com>
2020-05-02 04:28:03 +0300

    socket fix

--- src/main.js
+++ src/main.js
@@ -5,19 +5,8 @@ import router from '../server/router/index';
 import vuetify from '../plugins/vuetify';
 import PerfectScrollbar from 'vue2-perfect-scrollbar';
 import 'vue2-perfect-scrollbar/dist/vue2-perfect-scrollbar.css';
-import VueSocketIO from 'vue-socket.io'
-import SocketIO from "socket.io-client"
 Vue.use(PerfectScrollbar);
-Vue.use(new VueSocketIO({
-    debug: true,
-    connection: SocketIO('https://devert.ee:3000'), //options object is Optional
-    vuex: {
-      store,
-      actionPrefix: "SOCKET_",
-      mutationPrefix: "SOCKET_"
-    }
-  })
-);
+
 Vue.config.productionTip = false;
 
 new Vue({
